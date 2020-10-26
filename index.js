65bb7acd323ddf993a25d97eacb4f2ba65343891
reisnayara <reis_nayara@outlook.com>
2020-10-26 19:39:31 +0000

    Fixe port

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require ('express');
 const app = express ();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const hostname = '0.0.0.0';
 
 app.get('/', (req, res)=>{
     res.send('Hello World');
 
 });
 
-app.listen(port, ()=>{
-    console.log (`App Listening at http://localhost:${port}`);
-    
+app.listen(port, hostname, ()=>{
+    console.log (`App Listening at http://${hostname}:${port}`);
+
 })
